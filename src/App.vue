a81fc58ad612bb6fc82a0f1e73330759c9bef6d4
parkpoomd <parkpoom.d@gmail.com>
2020-12-19 11:20:28 +0700

    notifications

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,77 @@
 <template>
-  <h1 class="bg-indigo-600">App</h1>
+  <div class="p-8">
+    <span class="inline-flex rounded-md shadow-sm">
+      <button
+        @click="notify"
+        type="button"
+        class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      >
+        Notify
+      </button>
+    </span>
+  </div>
+  <div
+    class="fixed inset-0 flex items-start justify-end px-4 py-6 sm:p-6 pointer-events-none"
+  >
+    <div class="max-w-sm w-full">
+      <transition-group
+        tag="div"
+        :enter-active-class="
+          sortedNotifications.length > 1
+            ? 'transform ease-out delay-300 duration-300 transition'
+            : 'transform ease-out duration-300 transition'
+        "
+        enter-from-class="translate-x-4 opacity-0"
+        enter-to-class="translate-x-0 opacity-100"
+        leave-active-class="transition ease-in duration-100"
+        leave-from-class="opacity-100"
+        leave-to-class="opacity-0"
+        move-class="transition ease-in-out duration-500"
+      >
+        <Notification
+          v-for="(notification, i) in sortedNotifications"
+          :key="notification.id"
+          :id="notification.id"
+          :class="i > 0 ? 'mt-4' : ''"
+          @remove="removeNotification"
+        />
+      </transition-group>
+    </div>
+  </div>
 </template>
 
 <script>
+import Notification from './components/Notification';
+
 export default {
   name: 'App',
+  components: {
+    Notification,
+  },
+  data() {
+    return {
+      count: 0,
+      notifications: [],
+    };
+  },
+  computed: {
+    sortedNotifications() {
+      return [...this.notifications].reverse().slice(0, 4);
+    },
+  },
+  methods: {
+    removeNotification(id) {
+      this.notifications = this.notifications.filter((n) => n.id !== id);
+    },
+    notify() {
+      const notification = { id: this.count };
+      this.notifications.push(notification);
+      this.count += 1;
+
+      setTimeout(() => {
+        this.removeNotification(notification.id);
+      }, 3000);
+    },
+  },
 };
 </script>
---
+++ src/components/Notification.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="bg-white shadow-lg rounded-lg pointer-events-auto">
+    <div class="rounded-lg border overflow-hidden">
+      <div class="p-4">
+        <div class="flex items-start">
+          <div class="flex-shrink-0">
+            <svg
+              class="h-6 w-6 text-green-400"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </div>
+          <div class="ml-3 w-0 flex-1 pt-0.5">
+            <p class="text-sm leading-5 font-medium text-gray-900">
+              {{ id }}
+              Successfully saved!
+            </p>
+            <p class="mt-1 text-sm leading-5 text-gray-500">
+              Anyone with a link can now view this file.
+            </p>
+          </div>
+          <div class="ml-4 flex-shrink-0 flex">
+            <button
+              @click="$emit('remove', id)"
+              type="button"
+              class="inline-flex text-gray-400 focus:ouline-none focus:text-gray-500 transition duration-150 ease-in-out"
+            >
+              <svg
+                class="h-5 w-5"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['id'],
+};
+</script>
